8c5a9cac40f6362d7e11b734bf0e9e2fee964330
Christopher Doris <github.com/cjdoris>
2022-07-26 11:22:00 +0100

    coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,10 @@ using Test, Countries
 
     # for code coverage (which doesn't measure things only run during precompile)
     Countries._parse_json(Countries.Country, "3166-1")
+    Countries._parse_json(Countries.CountrySubdivision, "3166-2")
+    Countries._parse_json(Countries.Currency, "4217")
+    Countries._parse_json(Countries.Language, "639-3")
+    Countries._parse_json(Countries.Script, "15924")
 
     @test Countries.all_countries isa Vector{Countries.Country}
     @test Countries.all_country_subdivisions isa Vector{Countries.CountrySubdivision}
