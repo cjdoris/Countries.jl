dc45ecf34a5f2288937cf43b7a3445200d401350
Jake Ireland <jakewilliami@icloud.com>
2022-07-31 12:42:46 +1200

    Add tests for lookup functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Test, Countries
 
-@testset "Countries" begin
+@testset "Countries.jl" begin
 
     # for code coverage (which doesn't measure things only run during precompile)
     Countries._parse_json(Countries.Country, "3166-1")
@@ -8,11 +8,20 @@ using Test, Countries
     Countries._parse_json(Countries.Currency, "4217")
     Countries._parse_json(Countries.Language, "639-3")
     Countries._parse_json(Countries.Script, "15924")
-
-    @test Countries.all_countries isa Vector{Countries.Country}
-    @test Countries.all_country_subdivisions isa Vector{Countries.CountrySubdivision}
-    @test Countries.all_currencies isa Vector{Countries.Currency}
-    @test Countries.all_languages isa Vector{Countries.Language}
-    @test Countries.all_scripts isa Vector{Countries.Script}
-
+    
+    @testset "ISO Lists" begin
+        @test Countries.all_countries isa Vector{Countries.Country}
+        @test Countries.all_country_subdivisions isa Vector{Countries.CountrySubdivision}
+        @test Countries.all_currencies isa Vector{Countries.Currency}
+        @test Countries.all_languages isa Vector{Countries.Language}
+        @test Countries.all_scripts isa Vector{Countries.Script}
+    end
+    
+    @testset "ISO Lookup" begin
+        @test Countries.lookup_country(:alpha2, "BE").flag == "🇧🇪"
+        @test Countries.lookup_country_subdivision(:code, "SC-18").name == "Mont Fleuri"
+        @test Countries.lookup_currency(:alpha3, "EUR").name == "Euro"
+        @test Countries.lookup_language(:alpha2, "cy").name == "Welsh"
+        @test Countries.lookup_script(:alpha4, "Hung").numeric == Int16(176)
+    end
 end
